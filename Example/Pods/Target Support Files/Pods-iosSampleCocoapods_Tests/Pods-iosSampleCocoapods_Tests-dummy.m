f5631531fb7b47720939da532e7f59180cc0ffa2
Ravindra Kumar Saxena <ravindra.saxena@evry.com>
2017-10-18 14:11:16 +0530

    Add project via cocoapods

---
+++ Example/Pods/Target Support Files/Pods-iosSampleCocoapods_Tests/Pods-iosSampleCocoapods_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iosSampleCocoapods_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_iosSampleCocoapods_Tests
+@end
---
+++ Example/Pods/Target Support Files/iosSampleCocoapods/iosSampleCocoapods-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_iosSampleCocoapods : NSObject
+@end
+@implementation PodsDummy_iosSampleCocoapods
+@end
